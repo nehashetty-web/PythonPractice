76b0f7461d67aaae619c0f3cb1a4c777858ddc85
nehashetty-web <nehasree542@gmail.com>
2026-01-04 21:13:08 +0530

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,12 @@
+def returnValueFunction(n):
+    c=n*2
+    return c
+
+returnValueFunction(6)
+
+#Write the helloFunction below. It should have one statement
+#print("Hello")
+#code here
+def helloFunction():
+    print("Hello")
+helloFunction()
