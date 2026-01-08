8dfe8ddbb813aaee53274af555c3992d35ac16f2
nehashetty-web <nehasree542@gmail.com>
2026-01-08 23:03:43 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+'''Create a class with two instance variables using __init__(). Create two objects and
+ change one object’s variable. Print both.
+Create a class with one static variable and one instance variable.
+ Create two objects and change the static variable. Print the values.
+Create a class with one instance variable. Create two objects with different values.
+ Change one object’s value and print both.
+Create a class with one static variable and one instance variable. Create two objects
+ and modify the static variable. Print values for both objects.
+Create a class using __init__() with two instance variables. Create three objects and 
+change one variable of one object. Print all object values.'''
+"solutions"
