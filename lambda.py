14fea855f4738dd5bc29cc5e007d51d134aaf711
nehashetty-web <nehasree542@gmail.com>
2026-01-12 05:38:41 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,11 @@
+# 1. Lambda function to add two numbers
+add = lambda a, b: a + b
+print(add(10, 20))
+
+
+# 2. Lambda function to find square of a number
+square = lambda x: x * x
+print(square(5))
+# 3. Lambda function to check even or odd
+even_odd = lambda n: "Even" if n % 2 == 0 else "Odd"
+print(even_odd(7))
