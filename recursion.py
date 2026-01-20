67a7ef58eac61d03bee3680e3bdb5503dbeebec4
nehashetty-web <nehasree542@gmail.com>
2026-01-21 05:14:28 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -7,3 +7,12 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 
 print(fib(5))
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(5))
