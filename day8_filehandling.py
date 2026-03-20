8ee211d3c170e8bf7fa8d33fc3522848ef6edc82
nehashetty-web <nehasree542@gmail.com>
2026-03-20 23:07:13 +0530

    Update day8_filehandling.py

--- day8_filehandling.py
+++ day8_filehandling.py
@@ -1,11 +1,1 @@
-file=open("data.txt","w")
-file.write("hello neha")
-file.close()
-
-file=open("data.txt","r")
-print(file.read())
-file.close()
-
-file=open("data.txt","a")
-file.write("\n welcome")
-file.close()
+ 
