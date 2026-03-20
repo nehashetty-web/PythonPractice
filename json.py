a4003ce246babc793ff4115895bc49927a89e5ce
nehashetty-web <nehasree542@gmail.com>
2026-03-20 23:07:01 +0530

    Create json.py

---
+++ json.py
@@ -0,0 +1,11 @@
+import json
+student = {
+    "name": "Neha",
+    "marks": 95,
+    "course": "Python"
+}
+json_student=json.dumps(student)
+print(json_student)
+json.dump(student,student.json)
+json.load(student.json)
+print(marks)
